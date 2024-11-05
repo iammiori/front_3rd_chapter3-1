fa11717a44f50fb73f17ecf51d379cd769f8aa94
iammiori <immioriii@gmail.com>
2024-11-06 01:38:11 +0900

    test: getEventsForDay util 함수 테스트

--- src/__tests__/unit/easy.dateUtils.spec.ts
+++ src/__tests__/unit/easy.dateUtils.spec.ts
@@ -1,4 +1,11 @@
-import { getDaysInMonth, getWeekDates, getWeeksAtMonth, isLeapYear } from '../../utils/dateUtils';
+import { Event } from '../../types';
+import {
+  getDaysInMonth,
+  getEventsForDay,
+  getWeekDates,
+  getWeeksAtMonth,
+  isLeapYear,
+} from '../../utils/dateUtils';
 
 // 윤년 판단을 위해 추가한 테스트 코드
 describe('isLeapYear', () => {
@@ -148,13 +155,81 @@ describe('getWeeksAtMonth', () => {
 });
 
 describe('getEventsForDay', () => {
-  it('특정 날짜(1일)에 해당하는 이벤트만 정확히 반환한다', () => {});
+  const mockEvents: Event[] = [
+    {
+      id: 'da3ca408-836a-4d98-b67a-ca389d07552b',
+      title: '프로젝트 마감',
+      date: '2024-11-25',
+      startTime: '09:00',
+      endTime: '18:00',
+      description: '분기별 프로젝트 마감',
+      location: '사무실',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 1,
+    },
+    {
+      id: 'dac62941-69e5-4ec0-98cc-24c2a79a7f81',
+      title: '생일 파티',
+      date: '2024-11-28',
+      startTime: '19:00',
+      endTime: '22:00',
+      description: '친구 생일 축하',
+      location: '친구 집',
+      category: '개인',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 1,
+    },
+    {
+      id: '80d85368-b4a4-47b3-b959-25171d49371f',
+      title: '운동',
+      date: '2024-11-01',
+      startTime: '18:00',
+      endTime: '19:00',
+      description: '주간 운동',
+      location: '헬스장',
+      category: '개인',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 1,
+    },
+  ];
+  it('특정 날짜(1일)에 해당하는 이벤트만 정확히 반환한다', () => {
+    const foundEvents = getEventsForDay(mockEvents, 1);
+    expect(foundEvents).toHaveLength(1);
+    expect(foundEvents[0]).toEqual({
+      id: '80d85368-b4a4-47b3-b959-25171d49371f',
+      title: '운동',
+      date: '2024-11-01',
+      startTime: '18:00',
+      endTime: '19:00',
+      description: '주간 운동',
+      location: '헬스장',
+      category: '개인',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 1,
+    });
+  });
 
-  it('해당 날짜에 이벤트가 없을 경우 빈 배열을 반환한다', () => {});
+  it('해당 날짜에 이벤트가 없을 경우 빈 배열을 반환한다', () => {
+    const result = getEventsForDay(mockEvents, 6);
 
-  it('날짜가 0일 경우 빈 배열을 반환한다', () => {});
+    expect(result).toHaveLength(0);
+    expect(result).toEqual([]);
+  });
 
-  it('날짜가 32일 이상인 경우 빈 배열을 반환한다', () => {});
+  it('날짜가 0일 경우 빈 배열을 반환한다', () => {
+    const result = getEventsForDay(mockEvents, 0);
+
+    expect(result).toHaveLength(0);
+    expect(result).toEqual([]);
+  });
+
+  it('날짜가 32일 이상인 경우 빈 배열을 반환한다', () => {
+    const result = getEventsForDay(mockEvents, 32);
+
+    expect(result).toHaveLength(0);
+    expect(result).toEqual([]);
+  });
 });
 
 describe('formatWeek', () => {
