0bddd2022699c57aa12630b6d0b37d7815a25a1e
iammiori <immioriii@gmail.com>
2024-11-08 09:20:59 +0900

    feat: add mocking handler

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -6,11 +6,38 @@ import { events } from './response/events.json' assert { type: 'json' };
 // ! HARD
 // ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
 export const handlers = [
-  http.get('/api/events', () => {}),
+  http.get('/api/events', () => {
+    return HttpResponse.json({ events });
+  }),
 
-  http.post('/api/events', async ({ request }) => {}),
+  http.post('/api/events', async ({ request }) => {
+    const response = await request.json();
 
-  http.put('/api/events/:id', async () => {}),
+    const newEvent = { ...(response as Event), id: String(events.length + 1) };
 
-  http.delete('/api/events/:id', ({ params }) => {}),
+    return HttpResponse.json(newEvent, { status: 201 });
+  }),
+
+  http.put('/api/events/:id', async ({ params, request }) => {
+    const { id } = params;
+
+    const found = events.find((e) => e.id === id);
+    if (!found) {
+      return HttpResponse.json({ error: '이벤트 없음' }, { status: 404 });
+    }
+
+    const updatedEvent = (await request.json()) as Event;
+
+    return HttpResponse.json({ ...found, ...updatedEvent });
+  }),
+
+  http.delete('/api/events/:id', ({ params }) => {
+    const { id } = params;
+
+    const found = events.find((e) => e.id === id);
+    if (!found) {
+      return HttpResponse.json({ error: '이벤트 없음' }, { status: 404 });
+    }
+    return HttpResponse.json({ message: '삭제 완료!' }, { status: 204 });
+  }),
 ];
